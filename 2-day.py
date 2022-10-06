7a97be42d0f09f7e23241779a4b8e2356ffd5454
Lolarsky2 <98171019+Lolarsky2@users.noreply.github.com>
2022-10-06 00:39:17 -0800

    game-fix

--- 2-day.py
+++ 2-day.py
@@ -1,4 +1,5 @@
 # A Tip Calculator
+
 print("Welcome to the tip calculator!")
 total_bill = float(input("What was the total bill? $ "))
 percentage_tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
