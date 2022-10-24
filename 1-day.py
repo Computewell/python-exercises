fa3361491cb12f3b938120fba6b2f4631fe926fc
Lolarsky2 <98171019+Lolarsky2@users.noreply.github.com>
2022-10-24 14:42:43 +0100

    Hello world, Temperature conversion, Odd & Even, Area & volume, Fizz Buzz, Ordinal Suffix, Ascii Table, Read Write File

--- 1-day.py
+++
@@ -1,7 +0,0 @@
-# A Band Name Generator
-
-print("Welcome to the Band Name Generator")
-user_city = input("Which city did you grow up in ?\n")
-user_pet = input("What is the name of your pet ?\n")
-band_name = user_city + user_pet
-print(f"Your Band Name is {band_name}")
